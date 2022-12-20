6abd219b5ccb06d9a198d6b0a8ff65c5137914c3
Felix <77823268+felixny@users.noreply.github.com>
2022-12-19 22:14:46 -0500

    edited MainActivity.kt

--- app/src/main/java/edu/neu/madcourse/weatherapp/MainActivity.kt
+++ app/src/main/java/edu/neu/madcourse/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.neu.madcourse.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// main entry
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
